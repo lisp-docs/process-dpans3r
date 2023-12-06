1e78b2f9dcd5503fc281a89639ca6fb7263eed00
Daniel Nussenbaum <daninus14@gmail.com>
2023-12-06 15:35:34 +0200

    added to dos and some code

--- additional-text-fixes.py
+++ additional-text-fixes.py
@@ -49,8 +49,33 @@ def remove_double_lines_from_code_blocks(filename, root):
         file.write(curr_text)
         file.close()
 
+def fix_symbols_in_code_blocks(filename, root):
+    print("&lt;")
+    # TODO see ▷
+    # TODO see http://localhost:3000/cl-language-reference/docs/chap-5/f-b-generalized-reference#51121-examples-of-setf-expansions
+    # The example named sections may have figures, each figure is a code block... should not be hard to parse...
+    filepath = os.path.join(root, filename)
+    file = open(filepath, "r")
+    file_text = file.read()
+    file.close()
+    code_block_regex = r'```lisp[^```]+```'
+    code_blocks_found = re.findall(code_block_regex, file_text)
+    curr_text = file_text
+    for code_block in code_blocks_found:
+        new_code_block = code_block.replace("&lt;", "<")
+        new_code_block = new_code_block.replace("&gt;", ">")
+        curr_text = curr_text.replace(code_block, new_code_block)
+
+    if curr_text != file_text:
+        print(filepath)
+        # import pdb; pdb.set_trace()
+        file = open(filepath, "w")
+        file.write(curr_text)
+        file.close()
+
 def main():
-    process_all_md_files(MD_DIR, remove_double_lines_from_code_blocks)
+    # process_all_md_files(MD_DIR, remove_double_lines_from_code_blocks)
+    process_all_md_files(MD_DIR, fix_symbols_in_code_blocks)
     # find_non_ascii_chars_in_dir("../../../../../Learning/LISP/cl-drafts/lisp-docs-spec/new-spec/spec-source-pdf/output/chapters/docx/")
 
 if __name__ == "__main__":

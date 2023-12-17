bb7f2a9369bbfa1b12a80e549cb1915719661dc7
Daniel Nussenbaum <daninus14@gmail.com>
2023-12-17 16:11:07 +0200

    new changes to add code blocks correctly

--- add-md-code-blocks.py
+++ add-md-code-blocks.py
@@ -13,7 +13,7 @@ def process_files_in_dir(given_dir):
             if filename != ".DS_Store":
                 add_md_code_blocks(filename, root)
 
-def apply_first_example_code_block(given_text):
+def apply_first_example_code_block(given_text, filename):
     # There are markdown code blocks in the text already
     if given_text.find(MD_CODE_BEGIN) != -1:
         return given_text
@@ -23,24 +23,6 @@ def apply_first_example_code_block(given_text):
     # No examples section
     if example_index == -1:
         return given_text
-    
-    # figure_regex = r'\|(?:(?!\|)[^\|])*\|[\s\n]*\|\s*:-\s*\|[\s\n]+\*\*Figure \d+(–\d+)*\.[\w\s]*\*\*'
-    figure_regex = r'\|(?P<lisp_code>(?:(?!\|)[^\|])*)\|[\s\n]*\|\s*:-\s*\|[\s\n]+\*\*(?P<figure_name>Figure \d+(–\d+)*\.[\w\s]*)\*\*'
-    
-    # case where there are Figure examples
-    matches = [match for match in re.finditer(figure_regex, given_text)]
-    if len(matches) > 0:
-        new_text = ""
-        prev_end = 0
-        for match in matches:
-            (start, end) = match.span()
-            title = match.groupdict()["figure_name"]
-            lisp_code = match.groupdict()["lisp_code"]
-            new_text += given_text[prev_end: start]
-            new_text += MD_CODE_BLOCK_TITLE.format(title, lisp_code)
-            prev_end = end
-        new_text += given_text[prev_end:]
-        return new_text
     
     # Regular case
     post_example_index = example_index + len(example_title)
@@ -61,8 +43,29 @@ def conditionally_add_filename_codeblock(filename, root):
         file = open(filepath, "r")
         text = file.read()
         file.close()
-        if text.find(MD_CODE_BEGIN) == -1:
+        # figure_regex = r'\|(?:(?!\|)[^\|])*\|[\s\n]*\|\s*:-\s*\|[\s\n]+\*\*Figure \d+(–\d+)*\.[\w\s]*\*\*'
+        figure_regex = r'\|(?P<lisp_code>(?:(?!\|)[^\|])*)\|[\s\n]*\|\s*:-\s*\|[\s\n]+\*\*(?P<figure_name>Figure \d+(–\d+)*\.[\w\s]*)\*\*'
+        
+        # case where there are Figure examples
+        if "_c-d-b-b-c-examples-of-potential-numbers" in filename:
+            import pdb; pdb.set_trace()
+        matches = [match for match in re.finditer(figure_regex, text)]
+        code_block_wrapped_text = ""
+        if len(matches) > 0:
+            prev_end = 0
+            for match in matches:
+                (start, end) = match.span()
+                title = match.groupdict()["figure_name"]
+                lisp_code = match.groupdict()["lisp_code"]
+                code_block_wrapped_text += text[prev_end: start]
+                code_block_wrapped_text += MD_CODE_BLOCK_TITLE.format(title, lisp_code)
+                prev_end = end
+            code_block_wrapped_text += text[prev_end:]
+        
+        elif text.find(MD_CODE_BEGIN) == -1:
             code_block_wrapped_text = f'{MD_CODE_BEGIN}\n{text}\n{MD_CODE_END}'
+        
+        if len(code_block_wrapped_text) != 0:
             file = open(filepath, "w")
             file.write(code_block_wrapped_text)
             file.close()
@@ -86,15 +89,15 @@ def conditionally_add_section_codeblock(filename, root):
         text_parts.append(given_text[start: i])
         start = i
     text_parts.append(given_text[start:])
-    text_w_code_blocks = "".join([apply_first_example_code_block(text_part) for text_part in text_parts])
+    text_w_code_blocks = "".join([apply_first_example_code_block(text_part, filename) for text_part in text_parts])
     file = open(filepath, "w")
     file.write(text_w_code_blocks)
     file.close()
 
 def add_md_code_blocks(filename, root):
     # given_text
-    conditionally_add_filename_codeblock(filename, root)
     conditionally_add_section_codeblock(filename, root)
+    conditionally_add_filename_codeblock(filename, root)
 
 
 def main(args=[MD_DIR]):

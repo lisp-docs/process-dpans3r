975561a9f5ec780056be244320f9099f40509a06
Daniel Nussenbaum <daninus14@gmail.com>
2023-12-18 12:40:57 +0200

    cleaning dicionary file

--- code-blocks.py
+++ code-blocks.py
@@ -5,15 +5,6 @@ TEX_DIR = "./tex-files"
 MD_DIR = "./output/"
 CODE_BLOCKS_JSON = "./output/code-blocks.json"
 
-def get_block_list():
-    # for root, dirs, filenames in os.walk(TEX_DIR):
-    #     for filename in filenames:
-    #         if filename.endswith(".tex"):
-    #             print(filename)
-    #             print(os.path.join(root, filename))
-    with open(CODE_BLOCKS_JSON, "r") as filehandle:
-        return json.load(filehandle)
-
 def replace_code_blocks(code_blocks, given_dir):
     # print(os.listdir(given_dir))
     for root, dirs, filenames in os.walk(given_dir):
@@ -95,33 +86,21 @@ def split_dictionary_files(given_dir):
     # Loop through all files up to one depth
     # get the last file _x-x- or _x-x-x- where x.x.x would be the section number
     # check if it has a dictionary
+    # TODO check if dictionary already exists!
     # if it does: create a new file _x-(x+1)-dictionary.md
     # add title: make title be Chapter_name + Dictionary
     # save file
     # then later on modify the content and save the file...
     # the subsections always end with a colon **Notes:**. The titles don't have colons
-    # 
-    # print("split_dictionary_files")
     for root, dirs, filenames in os.walk(given_dir):
-        # print(root)
-        # print(dirs)
-        # print`([os.path.join(given_dir, dir) for dir in dirs].)
         # Chapters
         for dir in dirs:
-            # for sect
             curr_root = os.path.join(given_dir, dir)
             section_dir_names = [filename for filename in os.listdir(curr_root) if os.path.isdir(os.path.join(curr_root, filename))]
-            # for curr_root, chap_dirs, section_filenames in os.walk(os.path.join(given_dir, dir)):
-            # print(curr_root)
-            # print(section_dir_names)
             last_section = get_last_section(section_dir_names)
-            # print(last_section)
             if last_section:
-                # print(last_section)
                 last_sec_dir = os.path.join(curr_root, last_section)
                 last_file = get_last_section(os.listdir(last_sec_dir))
-                # print(last_sec_dir)
-                # print(last_file)
                 curr_file_path = os.path.join(last_sec_dir, last_file)
                 curr_file = open(curr_file_path, "r")
                 curr_text = curr_file.read()
@@ -361,15 +340,6 @@ def clear_footers(given_dir):
                         curr_file.write(curr_text)
                         curr_file.close()
                         
-def main(args=[]):
-    code_blocks = get_block_list()
-    # print(code_blocks[0][0])
-    # print("\n")
-    # print(code_blocks[0][1])
 
 if __name__ == "__main__":
-    # main(sys.argv[1:])
-    # replace_code_blocks([], MD_DIR)
     split_dictionary_files(MD_DIR)
-    # TODO Something is broken which is causing _shadow.md in chapter 11 to have a section repeated...
-    # clear_footers(MD_DIR)
